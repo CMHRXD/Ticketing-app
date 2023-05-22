e64e54bf8fbc98b907ed520a9cbb50334567954c
Cristian Hernandez <cmhrlxl@gmail.com>
2023-05-22 15:56:13 +0100

    Modify client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
+import { json } from 'react-router-dom'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  define: {
+    'process.env.STRIPE_TEST': 'pk_test_51MyC4IGmrvuw1mJ4vKgobsp14daeGSK5oP8ilL4jMX1VEsQFEyWgGqJIGFFYDMU7XyQEmTWJBHyN2diIZFihJg7G00jZpOOuS5',
   },
 })
