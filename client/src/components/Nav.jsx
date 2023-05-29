cb97d46345ddc9c151181273fedc72d5073d03bf
Cristian Hernandez <64647301+CMHRXD@users.noreply.github.com>
2023-05-29 10:37:15 +0100

    client modifications
    * Modify nav margin

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -42,7 +42,7 @@ const Nav = () => {
     <Disclosure as="nav" className="bg-blue-500">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-10">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
